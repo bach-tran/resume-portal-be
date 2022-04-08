8b0745c3857603a484d76e1a661377c88ec8c365
Bach Tran <bach_tran@outlook.com>
2022-04-07 23:47:14 -0500

    Remove test

--- src/test/java/com/bach/resumeportal/ResumePortalApplicationTests.java
+++ src/test/java/com/bach/resumeportal/ResumePortalApplicationTests.java
@@ -3,11 +3,11 @@ package com.bach.resumeportal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ResumePortalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ResumePortalApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
